cda95c925cb0ee8a2177d7c7038e096af7378601
Denis Roussel <droussel@centreon.com>
2022-01-03 11:59:07 +0100

    enh: Use typer instead of click

--- dkron_python/cli.py
+++ dkron_python/cli.py
@@ -1,51 +1,54 @@
-import click
 import json
 import os
-from .api import Dkron, DkronException
+from typing import List, Optional
+from dkron_python.api import Dkron, DkronException
+import typer
 
-_CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+app = typer.Typer()
+get = typer.Typer(help="Fetch information about a resource")
+apply = typer.Typer(help="Apply a resource")
+delete = typer.Typer(help="Delete a resource")
+app.add_typer(get, name="get")
+app.add_typer(apply, name="apply")
+app.add_typer(delete, name="delete")
+
 _DKRON_ENV_NAME_HOSTS = "DKRON_HOSTS"
 api = None
 
-
-@click.group(context_settings=_CONTEXT_SETTINGS)
-@click.option(
+HostsOption = typer.Option(
+    None,
     "-h",
     "--hosts",
     help="Dkron instance URLs, separated with commas",
     envvar=_DKRON_ENV_NAME_HOSTS,
 )
-@click.option(
+InsecureOption = typer.Option(
+    False,
     "-k",
     "--insecure",
+    help="Allow insecure connections when using SSL",
     is_flag=True,
-    help="Allow insecure server connections when using SSL",
 )
-def cli(hosts, insecure):
+JobName = typer.Argument(..., help="Name of the job")
+
+
+@app.callback()
+def cli(hosts: Optional[str] = HostsOption, insecure: bool = InsecureOption):
     """
     Command line interface client for Dkron
     """
     global api
     if not hosts:
         print(
-            "You must provide %s environment variable OR --hosts option"
-            % _DKRON_ENV_NAME_HOSTS
+            f"You must provide {_DKRON_ENV_NAME_HOSTS} environment variable OR --hosts option."
         )
         print("Check docs: https://github.com/centreon/dkron-python#cli-usage")
         exit(1)
     api = Dkron(hosts.split(","), verify=not insecure)
 
 
-@cli.group()
-def get():
-    """
-    Fetch information about resources
-    """
-    pass
-
-
-@get.command(name="status")
-def get_status():
+@get.command()
+def status():
     """
     Get system status
     """
@@ -57,8 +60,8 @@ def get_status():
     print(json.dumps(results))
 
 
-@get.command(name="leader")
-def get_leader():
+@get.command()
+def leader():
     """
     Get system leader
     """
@@ -70,8 +73,8 @@ def get_leader():
     print(json.dumps(results))
 
 
-@get.command(name="members")
-def get_members():
+@get.command()
+def members():
     """
     Get system members
     """
@@ -83,8 +86,8 @@ def get_members():
     print(json.dumps(results))
 
 
-@get.command(name="jobs")
-def get_jobs():
+@get.command()
+def jobs():
     """
     Fetch all jobs
     """
@@ -97,8 +100,7 @@ def get_jobs():
 
 
 @get.command(name="job")
-@click.argument("job_name")
-def get_job(job_name):
+def get_job(job_name: str = JobName):
     """
     Fetch specific job
     """
@@ -110,9 +112,8 @@ def get_job(job_name):
     print(json.dumps(results))
 
 
-@get.command(name="executions")
-@click.argument("job_name")
-def get_executions(job_name):
+@get.command()
+def executions(job_name: str = JobName):
     """
     Get system executions
     """
@@ -124,17 +125,10 @@ def get_executions(job_name):
     print(json.dumps(results))
 
 
-@cli.group()
-def apply():
-    """
-    Create or update resources
-    """
-    pass
-
-
 @apply.command(name="job")
-@click.argument("json_file_path", nargs=-1)
-def apply_job(json_file_path):
+def apply_job(
+    json_file_path: List[str] = typer.Argument(..., help="Path to the json file")
+):
     """
     Create or update job(s)
     """
@@ -149,9 +143,8 @@ def apply_job(json_file_path):
             print("Processed: %s" % file_path)
 
 
-@cli.command(name="run")
-@click.argument("job_name")
-def run_job(job_name):
+@app.command()
+def run(job_name: str = JobName):
     """
     Execute job on demand
     """
@@ -162,9 +155,8 @@ def run_job(job_name):
         exit(1)
 
 
-@cli.command(name="export")
-@click.argument("backup_dir")
-def export(backup_dir):
+@app.command(name="export")
+def export(backup_dir: str = typer.Argument(..., help="Path to the backup directory")):
     """
     Exports all jobs to json files
     """
@@ -178,16 +170,7 @@ def export(backup_dir):
         exit(1)
 
 
-@cli.group()
-def delete():
-    """
-    Delete resources
-    """
-    pass
-
-
 @delete.command(name="job")
-@click.argument("job_name")
 def delete_job(job_name):
     """
     Delete job
@@ -197,3 +180,7 @@ def delete_job(job_name):
     except DkronException as ex:
         print("Error while deleteing: %s" % str(ex))
         exit(1)
+
+
+if __name__ == "__main__":
+    app()
